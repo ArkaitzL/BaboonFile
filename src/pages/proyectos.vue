de9a1ab2c13f1e77e1a6337ebc110308abcfa0be
Arkaitz <127892239+ArkaitzL@users.noreply.github.com>
2023-10-25 14:24:59 +0200

    proyectos

--- src/pages/proyectos.vue
+++ src/pages/proyectos.vue
@@ -10,10 +10,9 @@ import axios from "axios"
             <hr class="star-dark mb-5">
             <div class="row">
               <article v-for="repo in repositorios" :key="repo.id" class="col-md-6 col-lg-4">
-                <a class="d-block mx-auto portfolio-item bg1">
+                <div @click="infoRepo(repo)" class="d-block mx-auto portfolio-item bg1">
                   <h4 class="nombre txt1">{{ repo.name }}</h4>             
-                  <!-- <img class="img-fluid" src="/src/assets/fondo.png"> -->
-                </a>
+                </div>
               </article>
             </div>
         </div>
@@ -57,6 +56,44 @@ export default {
         console.error('Error al cargar datos desde la API: ', error);
       }
     },
+    infoRepo(repo) {
+      const style = getComputedStyle(document.documentElement);
+
+      Swal.fire({
+        html: `
+        <div class="repo-info">
+          <h1 class="repo-name">${repo.name}</h1>
+          <hr class="star-dark">
+          <div class="repo-description">${repo.description ? repo.description + ` Este repositorio ha sido creado con <spam class="link"> ${repo.language} </spam>.` : 'No hay descripción disponible.'}
+          </div>
+          <br>
+          <br>
+          <div class="repo-details">
+            <div class="repo-detail">
+              <p><strong>Fecha creacion:</strong> ${new Date(repo.created_at).toLocaleDateString()}</p>
+            </div>
+          </div>
+        </div>
+        `,
+        footer: `<a href="${repo.html_url}" target="_blank">Ver el proyecto</a>`,
+        width: 600,
+        padding: '3em',
+        showConfirmButton: false,
+        // confirmButtonColor: style.getPropertyValue('--bg1'),
+        // confirmButtonText: 'GitHub',
+        // showCancelButton: true,
+        // cancelButtonColor: style.getPropertyValue('--bg1'), 
+        // cancelButtonText: 'Salir',
+        // cancelButtonPosition: 'top-end',
+        color: style.getPropertyValue('--txt2'), 
+        background: style.getPropertyValue('--bg2') 
+      })
+      // .then((res) => {
+      //   if (res.isConfirmed) {
+      //     window.open(repo.html_url, '_blank');
+      //   }
+      // });
+    }
   }
 };
 </script>
